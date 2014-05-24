f64993c1dbc8671615047f1eb89ac531b1ba5cff
tux-mind <massimo.dragano@gmail.com>
2014-05-24 12:25:26 +0200

    fixed included asm header

--- include/private/libncp-atomic.h
+++ include/private/libncp-atomic.h
@@ -1,6 +1,6 @@
 #ifndef __PRIVATE_LIBNCP_ATOMIC_H__
 #define __PRIVATE_LIBNCP_ATOMIC_H__
 
-#include "private/asm-i386/atomic.h"
+#include "private/asm-generic/atomic.h"
 
 #endif	/* __PRIVATE_LIBNCP_ATOMIC_H__ */
